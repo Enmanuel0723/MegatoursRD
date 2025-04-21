75b44b1db8871cf58146afa57443015f4f5ff8d7
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-04-21 00:49:37 -0400

    Se creo la clase modelo

---
+++ MegatoursRD/Models/CarritoDetalle.cs
@@ -0,0 +1,24 @@
+﻿using MegatoursRD.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class CarritoDetalle
+{
+	[Key]
+	public int CarritoDetalleId { get; set; }
+
+	public int CarritoId { get; set; }
+	public Carrito? Carrito { get; set; }
+
+	public int DestinoId { get; set; }
+	public Destinos? Destino { get; set; }
+
+	public int CantAdultos { get; set; }
+	public int CantNinos { get; set; }
+
+	public double PrecioAdultos { get; set; }
+	public double PrecioNinos { get; set; }
+
+	[NotMapped]
+	public double Total => (CantAdultos * PrecioAdultos) + (CantNinos * PrecioNinos);
+}
